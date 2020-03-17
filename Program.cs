676590a29d193e748ffceb64e58a3e6164b1cf66
Jani Ojanen <jajuojan@gmail.com>
2020-03-17 23:36:05 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello_azure_devops_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Wöörld!");
+        }
+    }
+}
